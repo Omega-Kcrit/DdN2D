3d9352b1739049a40631d8421b2c8642abc614d4
Loquele <joviedo@edu.tecnocampus.cat>
2020-05-15 15:56:05 +0200

    TileMap Done

---
+++ DdN Juego2d/Assets/Scripts/Character.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Rigidbody2D rgb;
+    public float speed;
+
+    public Vector2 mov;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rgb.AddForce(movement * speed);
+    }
+}
