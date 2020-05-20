c2dfc65aee64d9800196cd18ac6bb7625629f886
Loquele <joviedo@edu.tecnocampus.cat>
2020-05-20 11:11:06 +0200

    Mapa, Subir niveles, Oro. SOLO FALTA TEMA EXPLODIUM

--- DdN Juego2d/Assets/Scripts/Character.cs
+++ DdN Juego2d/Assets/Scripts/Character.cs
@@ -2,11 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
     private Rigidbody2D rgb;
     public float speed;
+    
+    
+    private int coins=0;
+    public Text cointext;
+
+    private int level;
+    public Text Leveltext;
+    
+    public GridController gridController;
 
     public Vector2 mov;
     // Start is called before the first frame update
@@ -29,5 +40,25 @@ public class Character : MonoBehaviour
         
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
         rgb.AddForce(movement * speed);
+        if (Input.GetKeyDown(KeyCode.DownArrow)||Input.GetKeyDown(KeyCode.S))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)||Input.GetKeyDown(KeyCode.W))
+        {
+            gridController.onLevelUp();
+        }
+    }
+
+    public void addcoin()
+    {
+        coins++;
+        cointext.text = coins.ToString();
+    }
+    public void addLevel()
+    {
+        level++;
+        Leveltext.text = level.ToString();
     }
 }
--- DdN Juego2d/Assets/Scripts/GridController.cs
+++ DdN Juego2d/Assets/Scripts/GridController.cs
@@ -9,8 +9,8 @@ public class GridController : MonoBehaviour
 
 
     public Grid grid;
-    public Tilemap rocasCavar;
-    public TileBase rocaDura, rocaRota;
+    public Tilemap rocasCavar, muro,oro,Explodium,RocaDura;
+    public TileBase rocaEntera, rocaRota,RocaLava;
     public GameObject player;
     
 
@@ -31,17 +31,57 @@ public class GridController : MonoBehaviour
             if (Vector2.Distance(pos, player.transform.position) < 1.5f)
             {
                 Debug.Log("Rango");
-                if (rocasCavar.GetTile(grid.WorldToCell(pos)).Equals(rocaDura))
-                    rocasCavar.SetTile(grid.WorldToCell(pos), rocaRota);
-                else if(rocasCavar.GetTile(grid.WorldToCell(pos)).Equals(rocaRota))
+                if (rocasCavar.HasTile(grid.WorldToCell(pos)))
                 {
-                    rocasCavar.SetTile(grid.WorldToCell(pos), null);
-                    Debug.Log("destruido");
+                    Debug.Log("TIEN ROCA PARA CABAR");
+                    Debug.Log(rocasCavar.GetTile(grid.WorldToCell(pos)));
+                    if (rocasCavar.GetTile(grid.WorldToCell(pos))==rocaEntera)
+                    {
+                        rocasCavar.SetTile(grid.WorldToCell(pos), rocaRota);
+                        Debug.Log("Mitad Destruida");
+                    }else if(rocasCavar.GetTile(grid.WorldToCell(pos)).Equals(rocaRota))
+                    {
+                        rocasCavar.SetTile(grid.WorldToCell(pos), null);
+                        Debug.Log("destruido");
+                    }
+                }else if (oro.HasTile(grid.WorldToCell(pos)))
+                {
+                    player.GetComponent<Character>().addcoin();
                 }
+               
+                
             }
             
          //rocasCavar.HasTile(grid.WorldToCell(pos));
          
         }
     }
+
+
+
+    public void onLevelUp()
+    {
+        Vector3 posOld = player.transform.position;
+        Vector3 pos = player.transform.position+ new Vector3(0,1,0);
+        
+        if (rocasCavar.GetTile(grid.WorldToCell(pos))==null&&oro.GetTile(grid.WorldToCell(pos))==null&&
+            muro.GetTile(grid.WorldToCell(pos))==null&&Explodium.GetTile(grid.WorldToCell(pos))==null&&RocaDura.GetTile(grid.WorldToCell(pos))==null)
+        {
+            
+            Debug.Log("Puede Subir");
+            player.GetComponent<Character>().addLevel();
+            player.transform.position+=new Vector3(0,1,0);
+            for (int i = -15; i<15; i++)
+            {
+                muro.SetTile(grid.WorldToCell(posOld+new Vector3(i,0,0)), RocaLava);
+                
+            }
+            
+        }else
+            Debug.Log("No puedes subir");
+
+        
+        
+    }
+    
 }
